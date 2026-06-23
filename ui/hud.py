1afc504826b4ccab7daa77d4efedc555cc69f5ae
alyarenamath25 <alyarenamath25@student.uns.ac.id>
2026-06-23 14:18:53 +0700

    merapikan kode

--- ui/hud.py
+++ ui/hud.py
@@ -1,14 +1,12 @@
-# ui/hud.py
 import pygame
 from settings import FONT_VCR, SCREEN_WIDTH, WHITE
 
 class HUD:
     """HUD menggunakan font VCR OSD Mono dengan efek kedip merah saat salah."""
-    # Setup font & variabel kedip
     def __init__(self):
         self.font = pygame.font.Font(FONT_VCR, 36)
         
-        # Variabel efek kedip merah
+        # Efek kedip merah
         self.flash_timer = 0.0
         self.flash_duration = 0.5 # Durasi kedip merah (0.5 detik)
 
@@ -21,9 +19,8 @@ class HUD:
         if self.flash_timer > 0:
             self.flash_timer -= dt
 
-    # Render skor & waktu
     def draw(self, screen, score: int, timer_str: str):
-        # 1. Logika penentuan warna Skor (Bergantian Merah dan Putih saat flash_timer aktif)
+        # Warna Skor (Bergantian Merah dan Putih saat flash_timer aktif)
         if self.flash_timer > 0:
             if int(self.flash_timer * 10) % 2 == 0:
                 score_color = (255, 0, 0)      # Merah murni saat kedip
@@ -32,11 +29,11 @@ class HUD:
         else:
             score_color = WHITE                # Kembali ke warna normal (Putih)
 
-        # 2. Gambar Skor menggunakan warna yang dinamis
+        # Skor
         score_surf = self.font.render(f"SCORE: {score:05d}", True, score_color)
         screen.blit(score_surf, (20, 20))
         
-        # 3. Gambar Timer (tetap putih seperti aslinya)
+        # Timer (tetap putih seperti asli)
         timer_surf = self.font.render(timer_str, True, WHITE)
         align_x = SCREEN_WIDTH - timer_surf.get_width() - 170 
         screen.blit(timer_surf, (align_x, 20))
